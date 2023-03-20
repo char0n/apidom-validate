0eddb0722b11bb9ddbb919f1ab15d8a8a9186c69
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-03-20 22:34:03 +0100

    feat: print validation result as nice table

--- validate.cjs
+++ validate.cjs
@@ -1,5 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const { Console }  = require( 'node:console')
+const { Transform } = require( 'node:stream')
 const { getLanguageService } = require('@swagger-api/apidom-ls');
 const core = require('@actions/core');
 const { TextDocument } = require('vscode-languageserver-textdocument');
@@ -8,15 +10,40 @@ const { TextDocument } = require('vscode-languageserver-textdocument');
 const [, , definitionFile] = process.argv;
 const definitionFilePath = path.join('/github/workspace', definitionFile);
 const definitionFileContent = fs.readFileSync(definitionFilePath, { encoding:'utf8', flag:'r' });
-const languageService = getLanguageService();
+const languageService = getLanguageService({});
 const textDocument = TextDocument.create(definitionFile, 'apidom', 0, definitionFileContent);
 
-core.info(`\u001b[1mValidating file "${definitionFile}"`)
+const mapLine = (range) => `${range.start.line}:${range.start.character}`;
+const mapSeverity = (severity) => severity === 1
+  ? 'error'
+  : severity === 2
+  ? 'warning'
+  : severity === 3
+  ? 'information'
+  : 'hint';
+const mapDiagnostic = ({ range, severity, code, message }) => ({
+  line: mapLine(range),
+  severity: mapSeverity(severity),
+  code,
+  message,
+})
+const mapDiagnostics = (diagnostics) => {
+  const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } });
+  const logger = new Console({ stdout: ts });
+
+  logger.table(diagnostics.map(mapDiagnostic));
+  return (ts.read() || '').toString();
+};
 
 (async () => {
   const validationResult = await languageService.doValidation(textDocument);
-  core.info(validationResult);
+  const hasValidationErrors = validationResult.some((diagnostic) => diagnostic.severity === 1);
+
+  core.info(`\u001b[1mApiDOM lint ${definitionFile}\n`)
+  core.info(`\u001b[1m${definitionFile}`)
+  core.info(mapDiagnostics(validationResult))
+
+  if (hasValidationErrors) {
+    core.setFailed('\u001b[38;2;255;0;1mDefinition contains errors.');
+  }
 })();
-
-
-core.info('\u001b[1mDefinition successfully validated by ApiDOM Language Service');
